2a7a5c522a2b3bfe5636f1a26ee2f71696045d96
catbyte-io <catbyte.io@gmail.com>
2024-12-14 21:46:14 -0700

    Add main.js

---
+++ my-ctd-website/src/main.js
@@ -0,0 +1,24 @@
+import './style.css'
+import neonCat from './assets/NeonCat_Profile.jpg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${neonCat}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Welcome to the Moodlist!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
