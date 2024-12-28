e41c75d1778d45a2b41397a4d3b38ccd0d004c78
catbyte-io <catbyte.io@gmail.com>
2024-12-28 16:12:08 -0700

    Add bootstrap for styling and navbar

--- my-ctd-website/src/main.js
+++ my-ctd-website/src/main.js
@@ -1,23 +1,59 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './style.css'
 import './fonts.css'
 import neonCat from './assets/NeonCat_Profile.jpg'
-import viteLogo from '/vite.svg'
+import happyLogo from './assets/Happy.jpg'
+import angryLogo from './assets/Angry.jpg'
+import danceLogo from './assets/Dance.gif'
+import sophisticatedLogo from './assets/Sophisticated.jpg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Fixed navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    <a href="https://catbyte.dev" target="_blank">
+      <img src="${neonCat}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${neonCat}" class="logo vanilla" alt="JavaScript logo" />
+    <a href="https://catbyte.dev" target="_blank">
+      <img src="${happyLogo}" class="logo vanilla" alt="Happy logo" />
+    </a>
+    <a href="https://catbyte.dev" target="_blank">
+      <img src="${angryLogo}" class="logo cherry" alt="Angryy logo" />
+    </a>
+    <a href="https://catbyte.dev" target="_blank">
+      <img src="${danceLogo}" class="logo mint" alt="Dance logo" />
+    </a>
+    <a href="https://catbyte.dev" target="_blank">
+      <img src="${sophisticatedLogo}" class="logo ice" alt="Sophisticated logo" />
     </a>
     <h1>Welcome to the Moodlist!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="subtle">
-      Click on the Vite logo to learn more
+      Click on a mood to get a Spotify playlist
     </p>
   </div>
 `
